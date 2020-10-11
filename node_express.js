b9eb7e6d3d5782d39ad19c7647d1647ed1c60d6b
MakersAll8 <lixiao.shanghai@gmail.com>
2020-10-11 13:58:41 +1100

    using node express to serve

---
+++ node_express.js
@@ -0,0 +1,21 @@
+let express = require('express');
+let path = require('path');
+let app = express();
+let port = 9999
+
+let webroot = path.join(__dirname , 'build')
+
+//setting middleware
+app.use(express.static(webroot)); //Serves resources from public folder
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(webroot, 'index.html'));
+});
+
+app.get('*', function (req, res) {
+    res.redirect('/');
+});
+
+app.listen(port, ()=>{
+    console.log('hall pass api server is up on port '+port)
+})
